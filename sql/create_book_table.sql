0a9ae0ef887271c4c99a39a3a2b6e0d0b5e7e989
TongZ0515 <157053590+TongZ0515@users.noreply.github.com>
2024-12-03 22:01:11 -0500

    Updated sql file by changing the db and data name

--- sql/create_book_table.sql
+++ sql/create_book_table.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS songs;
-CREATE TABLE songs (
+DROP TABLE IF EXISTS books;
+CREATE TABLE books (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    artist TEXT NOT NULL,
+    author TEXT NOT NULL,
     title TEXT NOT NULL,
-    year INTEGER NOT NULL, --CHECK(year >= 1900),
+    year INTEGER NOT NULL CHECK(year >= 1900),
     genre TEXT NOT NULL,
     --duration INTEGER NOT NULL CHECK(duration > 0),
     --play_count INTEGER DEFAULT 0,
