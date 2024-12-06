90eec50e02a871d851007f8aad5848389e7fa203
TongZ0515 <157053590+TongZ0515@users.noreply.github.com>
2024-12-06 14:48:19 -0500

    Update the import route in sql_utils

--- book_collection/utils/sql_utils.py
+++ book_collection/utils/sql_utils.py
@@ -3,7 +3,7 @@ import logging
 import os
 import sqlite3
 
-from music_collection.utils.logger import configure_logger
+from book_collection.utils.logger import configure_logger
 
 
 logger = logging.getLogger(__name__)
@@ -11,7 +11,7 @@ configure_logger(logger)
 
 
 # load the db path from the environment with a default value
-DB_PATH = os.getenv("DB_PATH", "/app/sql/song_catalog.db")
+DB_PATH = os.getenv("DB_PATH", "/app/sql/book_catalog.db")
 
 
 def check_database_connection():
